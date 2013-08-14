dd61b37d0b61e6cc25bda98d3b49261bbc867741
Martin Kolinek <martin.kolinek@gmail.com>
2013-08-14 20:30:01 +0200

    Fixed various problems, most notably type class parameters nested in other types

--- macro/src/main/scala/deriving/DerivingImpl.scala
+++ macro/src/main/scala/deriving/DerivingImpl.scala
@@ -9,6 +9,42 @@ object DerivingImpl {
 	
 	def createClassDef[NewT, OldT, OldInst](c:Context)(to:c.Expr[NewT => OldT], from:c.Expr[OldT=>NewT], typeClass:c.universe.ClassSymbol, newInstance:c.Type, ev:c.Expr[OldInst]) = {
 		import c.universe._
+		
+		def info(msg:Any) = c.info(c.enclosingPosition, msg.toString, true)
+		
+		def isDeferred(sym: Symbol) = sym
+         .asInstanceOf[scala.reflect.internal.Symbols#Symbol]
+         .hasFlag(scala.reflect.internal.Flags.DEFERRED)
+		
+		val tupleSize = {
+			val base = List(
+				typeOf[(_, _)],
+				typeOf[(_, _, _)],
+				typeOf[(_, _, _, _)],
+				typeOf[(_, _, _, _, _)],
+				typeOf[(_, _, _, _, _, _)],
+				typeOf[(_, _, _, _, _, _, _)],
+				typeOf[(_, _, _, _, _, _, _, _)],
+				typeOf[(_, _, _, _, _, _, _, _, _)],
+				typeOf[(_, _, _, _, _, _, _, _, _, _)],
+				typeOf[(_, _, _, _, _, _, _, _, _, _, _)],
+				typeOf[(_, _, _, _, _, _, _, _, _, _, _, _)],
+				typeOf[(_, _, _, _, _, _, _, _, _, _, _, _, _)],
+				typeOf[(_, _, _, _, _, _, _, _, _, _, _, _, _, _)],
+				typeOf[(_, _, _, _, _, _, _, _, _, _, _, _, _, _, _)],
+				typeOf[(_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _)],
+				typeOf[(_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _)],
+				typeOf[(_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _)],
+				typeOf[(_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _)],
+				typeOf[(_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _)],
+				typeOf[(_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _)],
+				typeOf[(_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _)]
+				)
+			base.zipWithIndex.map {
+				case(tp, i) => tp.typeConstructor.typeSymbol -> (i+2)
+			}.toMap
+		} 
+		
 		val fromName = newTermName("$deriving$from")
 		val toName = newTermName("$deriving$to")
 		val fromTree = Ident(fromName)
@@ -16,40 +52,106 @@ object DerivingImpl {
 		val fromVal = ValDef(NoMods, fromName, TypeTree(from.actualType), from.tree)
 		val toVal = ValDef(NoMods, toName, TypeTree(to.actualType), to.tree)
 		
-		val basicTypeClassParam = typeClass.typeParams(0).typeSignature 
-		val typeClassParams = (for{
-				bs <- typeClass.baseClasses ++ List(typeClass)
-				if bs.isClass
-				cls = bs.asClass
-				param <- cls.typeParams
-				if(param.typeSignature =:= basicTypeClassParam)
-			} yield param).toSet
+		val basicTypeClassParam = typeClass.typeParams(0)
+		
+		def hasTypeClassParam(tp:Type):Boolean = {
+			if(tp.typeSymbol == basicTypeClassParam)
+				true
+			else {
+				tp match {
+					case ref:TypeRef => ref.args.exists(hasTypeClassParam(_))
+					case _ => false
+				}
+			} 
+		}
+		
+		def replaceType(t:Type):TypTree = {
+			if(hasTypeClassParam(t)) {
+				if(t.typeSymbol == basicTypeClassParam)
+					TypeTree(newInstance)
+				else {
+					t match {
+						case ref:TypeRef => {
+							val newArgs = ref.args.map(replaceType(_))
+							AppliedTypeTree(Ident(t.typeConstructor.typeSymbol), newArgs)
+						}
+					}
+				}
+			}
+			else
+				TypeTree(t)
+		}
+		
+		def fixUnapply(comp:Symbol, tp:TypeRef) = {
+			comp.typeSignature.member("unapply":TermName) match {
+			    case m:MethodSymbol => {
+			    	if(m.paramss.size != 1 || m.paramss(0).size != 1)
+			    		c.abort(c.enclosingPosition, s"unapply of $comp has wrong number of parameters")
+			    	val paramSymb = m.paramss(0)(0).typeSignature
+			    	val toSubst = paramSymb match {
+			    		case r:TypeRef => r.args.map(_.typeSymbol)
+			    		case _ => c.abort(c.enclosingPosition, "unapply parameter not a TypeRef")
+			    	}
+			    	m.returnType.substituteSymbols(toSubst, tp.args.map(_.typeSymbol))
+			    }
+			    case _ => c.abort(c.enclosingPosition, s"unapply of $comp is not a method")
+			}
+		}
+		
+		def transformTree(t:Tree, tp:Type, func:Tree):Tree = {
+			if(hasTypeClassParam(tp)) {
+				if(tp.typeSymbol == basicTypeClassParam)
+					Apply(func, List(t))
+				else {
+					tp match {
+						case ref:TypeRef => {
+							val comp = tp.typeSymbol.companionSymbol
+							val unappType = fixUnapply(comp, ref)
+							val tup = unappType.asInstanceOf[TypeRef].args(0).asInstanceOf[TypeRef]
+							val size = tupleSize(tup.typeConstructor.typeSymbol)
+							val params = (1 to size).map(nr => s"$$deriving$$param$$$nr").toList//.map(n => newTermName(n)).toList
+							val appliedParams = for((p, pt) <- params.map(x=>Ident(newTermName(x))).zip(tup.args)) yield transformTree(p, pt, func)
+							val cse = CaseDef(
+									Apply(Ident(comp), 
+											params.map(p => Bind(newTermName(p), Ident(nme.WILDCARD)))),
+									EmptyTree,
+									Apply(Select(Ident(comp), "apply":TermName), appliedParams)
+									)
+							Match(t, List(cse))
+						}
+					}
+				}
+			}
+			else
+				t
+		}
+		
+		val baseSymbols = (for {
+			bs <- typeClass.baseClasses.drop(1)
+			if bs.isClass
+			bstype = typeClass.typeSignature.baseType(bs)
+			(tp, symb) <- bstype.asInstanceOf[TypeRef].args zip bs.asClass.typeParams
+		} yield symb -> tp.typeSymbol).unzip
+		
+		def substituteBaseSymbols(tpe:Type) = {
+			tpe.substituteSymbols(baseSymbols._1, baseSymbols._2)
+		}
+		
+		def processReturnValue(t:Tree, retType:Type):Tree = transformTree(t, substituteBaseSymbols(retType), fromTree)
 		
 		def processParameters(params:List[Symbol]) = {
-			params.map {
-			    case x if typeClassParams.contains(x.typeSignature.typeSymbol) => 
-			        ValDef(NoMods, x.name.asInstanceOf[TermName], TypeTree(newInstance), EmptyTree) ->
-			        Apply(toTree, List(Ident(x.name)))
-			    case x => 
-			        ValDef(NoMods, x.name.asInstanceOf[TermName], TypeTree(x.typeSignature), EmptyTree) ->
-			        Ident(x.name)
-			}.unzip
+			params
+			    .map(x=>x.name -> substituteBaseSymbols(x.typeSignature))
+			    .map {
+			        case (name, tpe) if hasTypeClassParam(tpe) => 
+			            ValDef(NoMods, name.asInstanceOf[TermName], replaceType(tpe), EmptyTree) ->
+			            transformTree(Ident(name), tpe, toTree)
+			        case (name, tpe) =>  
+			            ValDef(NoMods, name.asInstanceOf[TermName], TypeTree(tpe), EmptyTree) ->
+			            Ident(name)
+			    }.unzip
 		}
 		
-		def processReturnValue(t:Tree, retType:Type) = {
-			if(typeClassParams.contains(retType.typeSymbol))
-				Apply(fromTree, List(t))
-			else
-				t
-		}
-			
-		def replaceType(t:Type) = {
-			if(typeClassParams.contains(t.typeSymbol))
-				newInstance
-			else
-				t
-		}
-			
 		val ctor = {
 			val constructorBody = Block(List(Apply(Select(Super(This(tpnme.EMPTY), tpnme.EMPTY), nme.CONSTRUCTOR), List())), Literal(Constant(())))
 			DefDef(NoMods, nme.CONSTRUCTOR, List(), List(List()), TypeTree(), constructorBody)
@@ -57,8 +159,7 @@ object DerivingImpl {
 			
 		val toChange = typeClass.typeSignature.members.collect {
 		    case x:MethodSymbol 
-		        if x.paramss.flatten.exists(param => typeClassParams.contains(param.typeSignature.typeSymbol))
-		            || typeClassParams.contains(x.returnType.typeSymbol)
+		        if isDeferred(x)
 		            => x
 		}
 			
@@ -66,7 +167,7 @@ object DerivingImpl {
 		    case x => {
 		    	val mods = Modifiers(Flag.OVERRIDE)
 		    	val name = x.name.asInstanceOf[TermName]
-		    	val tpt = TypeTree(replaceType(x.returnType))
+		    	val tpt = replaceType(substituteBaseSymbols(x.returnType))
 		    	val base:Tree = Select(ev.tree, name)
 		    	val (typeParamLists, paramLists) = x.paramss.map(processParameters(_)).unzip
 		    	val body = processReturnValue((base /: paramLists)(Apply(_, _)), x.returnType)
@@ -83,7 +184,7 @@ object DerivingImpl {
 				List(ctor, fromVal, toVal) ++ changed)
 		ClassDef(NoMods, newTypeName(s"${newInstance.typeSymbol.name}InstanceOf${typeClass.name}"), Nil, tmpl)
 	}
-	
+		
 	def macroImpl[NewT, OldT, NewInst, OldInst](c:Context)(to:c.Expr[NewT => OldT], from:c.Expr[OldT => NewT])(ev:c.Expr[OldInst])(implicit wttco:c.WeakTypeTag[OldInst], wtto:c.WeakTypeTag[OldT], wttn:c.WeakTypeTag[NewT], wttcn:c.WeakTypeTag[NewInst]) = {
 		import c.universe._
 		
--- src/test/scala/deriving/DerivingUtilTest.scala
+++ src/test/scala/deriving/DerivingUtilTest.scala
@@ -17,6 +17,32 @@ trait IsBetterCat[T] extends IsCat[T] {
 	def scratch(cat:T):Int
 }
 
+trait Nested[T,Q] {
+	def func(x:(T, T)):(T, T, Q)
+}
+
+trait BaseTypeClass[T, Q] {
+	def funcT(t:T, q:Q):T
+	def funcQ(t:T, q:Q):Q
+}
+
+trait SubTypeClass[T] extends BaseTypeClass[T, Int] {
+	def func(t:T):T
+}
+
+case class NestedCClass[T](a:T, b:T)
+
+trait BetterNested[T] extends Nested[T, Int] {
+	def func2(x:NestedCClass[T]):NestedCClass[T]
+}
+
+trait IsPrivateCat[T] extends IsCat[T] {
+	private def priv(t:T) = meow(t)
+	protected[this] def protthis(t:T) = meow(t)
+	def pub(t:T) = priv(t)
+	def pub2(t:T) = protthis(t)
+}
+
 object Instances {
 	implicit val catIsBetterCat = new IsBetterCat[Cat] {
 		def meow(cat:Cat) = "meow"
@@ -26,9 +52,10 @@ object Instances {
 }
 
 class DerivingUtilTest extends FunSuite {
-	import Instances._
+	
 	
 	test("deriving works") {
+		import Instances._
 		implicit val catInABoxIsCat = deriving[CatInABox, IsCat].equiv(_.cat, CatInABox)
 		
 		val cat = CatInABox(new Cat("cat"))
@@ -38,10 +65,54 @@ class DerivingUtilTest extends FunSuite {
 	}
 	
 	test("deriving works with inheritance") {
+		import Instances._
 		implicit val catInABoxIsBetterCat = deriving[CatInABox, IsBetterCat].equiv(_.cat, CatInABox)
 		val cat = CatInABox(new Cat("cat"))
 		assert(implicitly[IsCat[CatInABox]].meow(cat)=="meow")
 		assert(implicitly[IsCat[CatInABox]].mate(cat, cat).cat.name == "cat junior")
 		assert(implicitly[IsBetterCat[CatInABox]].scratch(cat)==4)
+	}
+	
+	test("deriving works with private members") {
+		implicit val catIsPrivateCat = new IsPrivateCat[Cat] {
+			def meow(cat:Cat) = "meow"
+			def mate(cat1:Cat, cat2:Cat) = new Cat(s"${cat1.name} junior")
+			protected def prot(cat:Cat) = "protected"
+		}
+		implicit val catInABoxIsPrivateCat = deriving[CatInABox, IsPrivateCat].equiv(_.cat, CatInABox)
+		val cat = CatInABox(new Cat("cat"))
+		assert(implicitly[IsPrivateCat[CatInABox]].pub(cat)=="meow")
+		assert(implicitly[IsPrivateCat[CatInABox]].pub2(cat)=="meow")
+	}
+	
+	test("deriving works with inheritance with subclass taking multiple type parameters") {
+		implicit val catIsInstanceOfSubTypeClass = new SubTypeClass[Cat] {
+			def func(c:Cat) = c
+			def funcT(c:Cat, i:Int) = c
+			def funcQ(c:Cat, i:Int) = i
+		}
+		
+		implicit val catInABoxIsInstanceOfSubTypeClass = deriving[CatInABox, SubTypeClass].equiv(_.cat, CatInABox)
+		
+		val inst = implicitly[SubTypeClass[CatInABox]]
+		assert(inst.func(CatInABox(new Cat("ferdinand"))).cat.name == "ferdinand")
+		assert(inst.funcT(CatInABox(new Cat("ferdinand")), 1).cat.name == "ferdinand")
+		assert(inst.funcQ(CatInABox(new Cat("ferdinand")), 2) == 2)
+	}
+	
+	test("deriving works with type classes containing type arguments nested in other types") {
+		implicit val catIsNested = new BetterNested[Cat] {
+			def func(x:(Cat, Cat)) = (x._1, x._2, 4)
+			def func2(x:NestedCClass[Cat]) = x
+		}
+		
+		implicit val catInABoxIsNested = deriving[CatInABox, BetterNested].equiv(_.cat, CatInABox)
+		
+		val kitty = new Cat("kitty")
+		val b = new Cat("b")
+		val inst = implicitly[BetterNested[CatInABox]]
+		assert(inst.func((CatInABox(kitty), CatInABox(b))) == (CatInABox(kitty), CatInABox(b), 4))
+		val cclass = NestedCClass(CatInABox(kitty), CatInABox(b))
+		assert(inst.func2(cclass) == cclass)
 	}
 }
