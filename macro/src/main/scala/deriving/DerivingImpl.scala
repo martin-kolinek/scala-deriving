2c05c6ebc120f9cd07d75c7639454fd7964ba9cb
Martin Kolinek <martin.kolinek@gmail.com>
2014-05-02 13:16:31 +0200

    Switch to scala 2.11

--- macro/src/main/scala/deriving/DerivingImpl.scala
+++ macro/src/main/scala/deriving/DerivingImpl.scala
@@ -1,18 +1,12 @@
 package deriving
 
-import scala.reflect.macros.Context
+import scala.reflect.macros.blackbox.Context
 import scala.language.higherKinds
-import scala.collection.mutable.ListBuffer
-import sun.reflect.generics.tree.ReturnType
-import sun.reflect.generics.tree.TypeSignature
-import com.sun.xml.internal.ws.policy.sourcemodel.NormalizedModelGenerator
 
 object DerivingImpl {
 	
 	def createClassDef[NewT, OldT, OldInst](c:Context)(to:c.Expr[NewT => OldT], from:c.Expr[OldT=>NewT], typeClass:c.universe.ClassSymbol, newInstance:c.Type, ev:c.Expr[OldInst]) = {
 		import c.universe._
-		
-		def info(msg:Any) = c.info(c.enclosingPosition, msg.toString, true)
 		
 		def isDeferred(sym: Symbol) = sym
          .asInstanceOf[scala.reflect.internal.Symbols#Symbol]
@@ -47,8 +41,8 @@ object DerivingImpl {
 			}.toMap
 		} 
 		
-		val fromName = newTermName("$deriving$from")
-		val toName = newTermName("$deriving$to")
+		val fromName = TermName("$deriving$from")
+		val toName = TermName("$deriving$to")
 		val fromTree = Ident(fromName)
 		val toTree = Ident(toName)
 		val fromVal = ValDef(NoMods, fromName, TypeTree(from.actualType), from.tree)
@@ -61,24 +55,23 @@ object DerivingImpl {
 				true
 			else {
 				tp match {
-					case ref:TypeRef => ref.args.exists(hasTypeClassParam(_))
+					case ref:TypeRef => ref.args.exists(hasTypeClassParam)
 					case _ => false
 				}
-			} 
+			}
 		}
 		
 		def hasIdentType(t:Type, identSymbols:Set[Symbol]):Boolean = {
 		    if(identSymbols.contains(t.typeSymbol))
 		        true
 		    else t match {
-		        case ref:TypeRef => {
+		        case ref:TypeRef =>
 		            ref.args.exists(hasIdentType(_, identSymbols))
-		        }
 		        case _ => false
 		    }
 		}
-		
-		def typeParamNewTypeName(s:Symbol) = newTypeName(s.name.decoded+"Deriv")
+
+		def typeParamNewTypeName(s:Symbol) = TypeName(s.name.decodedName.toString + "Deriv")
 		
 		def replaceType(t:Type, identSymbols:Set[Symbol]):Tree = {
 			if(hasTypeClassParam(t) || hasIdentType(t, identSymbols)) {
@@ -100,11 +93,11 @@ object DerivingImpl {
 		}
 		
 		def fixUnapply(comp:Symbol, tp:TypeRef) = {
-			comp.typeSignature.member("unapply":TermName) match {
+			comp.typeSignature.member(TermName("unapply")) match {
 			    case m:MethodSymbol => {
-			    	if(m.paramss.size != 1 || m.paramss(0).size != 1)
+			    	if(m.paramLists.size != 1 || m.paramLists(0).size != 1)
 			    		c.abort(c.enclosingPosition, s"unapply of $comp has wrong number of parameters")
-			    	val paramSymb = m.paramss(0)(0).typeSignature
+			    	val paramSymb = m.paramLists(0)(0).typeSignature
 			    	val toSubst = paramSymb match {
 			    		case r:TypeRef => r.args.map(_.typeSymbol)
 			    		case _ => c.abort(c.enclosingPosition, "unapply parameter not a TypeRef")
@@ -122,19 +115,19 @@ object DerivingImpl {
 				else {
 					tp match {
 						case ref:TypeRef => {
-							val comp = tp.typeSymbol.companionSymbol
+							val comp = tp.typeSymbol.companion
+							val compApply = comp.typeSignature.member(TermName("apply"))
 							val unappType = fixUnapply(comp, ref)
 							val tup = unappType.asInstanceOf[TypeRef].args(0).asInstanceOf[TypeRef]
 							val size = tupleSize(tup.typeConstructor.typeSymbol)
-							val params = (1 to size).map(nr => s"$$deriving$$param$$$nr").toList//.map(n => newTermName(n)).toList
-							val appliedParams = for((p, pt) <- params.map(x=>Ident(newTermName(x))).zip(tup.args)) yield transformTree(p, pt, func)
-							val cse = CaseDef(
-									Apply(Ident(comp), 
-											params.map(p => Bind(newTermName(p), Ident(nme.WILDCARD)))),
-									EmptyTree,
-									Apply(Select(Ident(comp), "apply":TermName), appliedParams)
-									)
-							Match(t, List(cse))
+							val paramNames = (1 to size).map(nr => s"deriving$$param$$$nr").map(TermName(_))
+							val patterns = paramNames.map(x => pq"$x @ _")
+							val params = paramNames.zip(tup.args).map(x => transformTree(q"${x._1}", x._2, func))
+							q"""
+							$t match {
+							    case $comp(..$patterns) => $comp.apply(..$params)
+						    }
+							"""
 						}
 					}
 				}
@@ -175,8 +168,8 @@ object DerivingImpl {
 		}
 		
 		val ctor = {
-			val constructorBody = Block(List(Apply(Select(Super(This(tpnme.EMPTY), tpnme.EMPTY), nme.CONSTRUCTOR), List())), Literal(Constant(())))
-			DefDef(NoMods, nme.CONSTRUCTOR, List(), List(List()), TypeTree(), constructorBody)
+			val constructorBody = Block(List(Apply(Select(Super(This(typeNames.EMPTY), typeNames.EMPTY), termNames.CONSTRUCTOR), List())), Literal(Constant(())))
+			DefDef(NoMods, termNames.CONSTRUCTOR, List(), List(List()), TypeTree(), constructorBody)
 		}
 			
 		val toChange = typeClass.typeSignature.members.collect {
@@ -195,8 +188,8 @@ object DerivingImpl {
 		    	val name = x.name.asInstanceOf[TermName]
 		    	val tpt = replaceType(substituteBaseSymbols(x.returnType), identSymbols)
 		    	val base:Tree = Select(ev.tree, name)
-		    	val paramValDefs = x.paramss.map(createParameterValDefs(_, identSymbols))
-		    	val paramTrees = x.paramss.map(createParameterTrees(_))
+		    	val paramValDefs = x.paramLists.map(createParameterValDefs(_, identSymbols))
+		    	val paramTrees = x.paramLists.map(createParameterTrees(_))
 		    	val body = processReturnValue((base /: paramTrees)(Apply(_, _)), x.returnType)
 		    	if(x.isVal) 
 		    		ValDef(mods, name, tpt, body)
@@ -207,9 +200,9 @@ object DerivingImpl {
 			
 		val newTypeClassCls = AppliedTypeTree(Ident(typeClass), List(TypeTree(newInstance)))
 		val tmpl:Template = Template(List(newTypeClassCls),
-				emptyValDef,
+				noSelfType,
 				List(ctor, fromVal, toVal) ++ changed)
-		ClassDef(NoMods, newTypeName(s"${newInstance.typeSymbol.name}InstanceOf${typeClass.name}"), Nil, tmpl)
+		ClassDef(NoMods, TypeName(s"${newInstance.typeSymbol.name}InstanceOf${typeClass.name}"), Nil, tmpl)
 	}
 		
 	def macroImpl[NewT, OldT, NewInst, OldInst](c:Context)(to:c.Expr[NewT => OldT], from:c.Expr[OldT => NewT])(ev:c.Expr[OldInst])(implicit wttco:c.WeakTypeTag[OldInst], wtto:c.WeakTypeTag[OldT], wttn:c.WeakTypeTag[NewT], wttcn:c.WeakTypeTag[NewInst]) = {
@@ -227,7 +220,7 @@ object DerivingImpl {
 		
 		val cdef = createClassDef(c)(to, from, cls, wttn.tpe, ev)
 		
-		val const = Select(New(Ident(cdef.name)), nme.CONSTRUCTOR)
+		val const = Select(New(Ident(cdef.name)), termNames.CONSTRUCTOR)
 		val block = Block(List(cdef), Apply(const, Nil))
 		c.Expr[NewInst](block)
 	}
--- project/Build.scala
+++ project/Build.scala
@@ -1,20 +1,25 @@
 import sbt._
 import Keys._
+import com.typesafe.sbteclipse.plugin.EclipsePlugin.EclipseKeys
 
 object DerivingBuild extends Build {
 	val buildSettings = Defaults.defaultSettings ++ Seq(
 		name:="scala-deriving",
+                organization:="org.kolinek",
 		version:="0.1",
-		scalaVersion:="2.10.2",
-		libraryDependencies += "org.scalatest" %% "scalatest" % "1.9.1" % "test",
-		scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature")
+		scalaVersion:="2.11.0",
+		libraryDependencies += "org.scalatest" %% "scalatest" % "2.1.5" % "test",
+		scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-Ymacro-debug-lite")
 	)
 	
-	lazy val main = Project("main", file("."), settings = buildSettings) dependsOn(macroSub)
+	lazy val main = Project("main", file("."), settings = buildSettings) aggregate(macroSub) dependsOn(macroSub)
 	
 	lazy val macroSub = Project("macro", file("macro")) settings (
 			name:="scala-deriving-macro",
-			scalaVersion:="2.10.2",
+                        organization:="org.kolinek",
+                        version:="0.1",
+			scalaVersion:="2.11.0",
 			scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature"),
-			libraryDependencies += "org.scala-lang" % "scala-reflect" % "2.10.2") 
+			libraryDependencies += "org.scala-lang" % "scala-reflect" % "2.11.0",
+                        EclipseKeys.skipParents in ThisBuild := false)
 }
