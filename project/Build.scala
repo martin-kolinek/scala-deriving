db9dcd21a4f80ec966b17dc4385037b174830d70
Martin Kolinek <martin.kolinek@gmail.com>
2014-05-03 19:23:00 +0200

    Version mixup fix

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ object DerivingBuild extends Build {
 	val buildSettings = Defaults.defaultSettings ++ Seq(
 		name:="scala-deriving",
                 organization:="org.kolinek",
-		version:="0.2.0",
+		version:="0.2.1",
 		scalaVersion:="2.11.0",
 		libraryDependencies += "org.scalatest" %% "scalatest" % "2.1.5" % "test",
 		scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-Ymacro-debug-lite")
@@ -17,7 +17,7 @@ object DerivingBuild extends Build {
 	lazy val macroSub = Project("macro", file("macro")) settings (
 			name:="scala-deriving-macro",
                         organization:="org.kolinek",
-                        version:="0.1",
+                        version:="0.2.1",
 			scalaVersion:="2.11.0",
 			scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature"),
 			libraryDependencies += "org.scala-lang" % "scala-reflect" % "2.11.0",
